829823a6314cf464d0eaa6e6ec048886e2c3b324
VersanMihai <versan.mihai@gmail.com>
2025-06-17 22:04:04 +0300

    add and fetch books apis

--- 6_frameworks/fast_api/book_store/books.py
+++ 6_frameworks/fast_api/book_store/books.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
 
 app = FastAPI()
 
-@app.get("/{name}")
-def read_api(name: str):
-    return {'Welcome': name}
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=101) # will be from 0-100
+
+books = []
+
+
+@app.get("/")
+def read_api():
+    return books
+
+@app.post("/")
+def create_book(book: Book):
+    books.append(book)
+    return book
