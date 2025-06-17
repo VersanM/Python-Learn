690afab5af251b0a01b6fc3c29ffdaf2a939c9da
VersanMihai <versan.mihai@gmail.com>
2025-06-17 22:16:17 +0300

    put + delete apis

--- 6_frameworks/fast_api/book_store/books.py
+++ 6_frameworks/fast_api/book_store/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from uuid import UUID
 
@@ -22,3 +22,32 @@ def read_api():
 def create_book(book: Book):
     books.append(book)
     return book
+
+@app.put("/{book_id}")
+def update_book(book_id: UUID, book: Book):
+    counter = 0
+
+    for x in books:
+        counter += 1
+        if x.id == book_id:
+            books[counter-1] = book
+            return books[counter-1]
+    raise HTTPException(
+        status_code=404,
+        detail=f"ID {book_id} - Does not exist"
+    )
+
+@app.delete("/{book_id}")
+def delete_book(book_id: UUID):
+    counter = 0
+
+    for x in books:
+        counter += 1
+        if x.id == book_id:
+            del books[counter-1]
+            return f"ID: {book_id} was deleted"
+    raise HTTPException(
+        status_code=404,
+        detail=f"ID {book_id} - Does not exist"
+    )
+
