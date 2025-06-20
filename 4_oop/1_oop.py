266b02a68e376b7889eb7b87e8f1186091d5d46d
VersanMihai <versan.mihai@gmail.com>
2025-06-20 19:58:47 +0300

    oop classes and constructors

---
+++ 4_oop/1_oop.py
@@ -0,0 +1,31 @@
+# class declaration
+class Enemy:
+    type_of_enemy: str
+    health_points: int = 100
+    attack_damage: int = 10
+
+    def talk(self):
+        print(f'I am a {self.type_of_enemy}. Be prepared to fight!')
+
+    def walk_forward(self):
+        print(f'{self.type_of_enemy} moves closer to you.')
+
+    def attack(self):
+        print(f'{self.type_of_enemy} attacks for {self.attack_damage} damage')
+
+
+zombie = Enemy()
+zombie.type_of_enemy = 'Zombie'
+print(f'{zombie.type_of_enemy} has {zombie.health_points} health points and can do an attack damage of {zombie.attack_damage}')
+
+# Abstraction
+print(zombie.talk())
+
+# Constructors
+class EnemyNew:
+    def __init__(self, type_of_enemy, health_points=100, attack_damage = 10):
+        self.type_of_enemy = type_of_enemy
+        self.health_points = health_points
+        self.attack_damage = attack_damage
+
+    
