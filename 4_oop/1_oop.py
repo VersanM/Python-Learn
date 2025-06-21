c9503e04d82679850c75a7b3e6d8f5cf3d8404fa
VersanMihai <versan.mihai@gmail.com>
2025-06-21 08:27:08 +0300

    oop constructor cont

--- 4_oop/1_oop.py
+++ 4_oop/1_oop.py
@@ -27,5 +27,16 @@ class EnemyNew:
         self.type_of_enemy = type_of_enemy
         self.health_points = health_points
         self.attack_damage = attack_damage
-
     
+    def talk(self):
+        print(f'I am a {self.type_of_enemy}. Be prepared to fight!')
+
+    def walk_forward(self):
+        print(f'{self.type_of_enemy} moves closer to you.')
+
+    def attack(self):
+        print(f'{self.type_of_enemy} attacks for {self.attack_damage} damage')
+
+
+zombie = EnemyNew(type_of_enemy='Zombie')
+zombie.talk()
