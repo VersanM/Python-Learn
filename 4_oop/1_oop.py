6475570489d30a27f9b34b938a41ec3932f356aa
VersanMihai <versan.mihai@gmail.com>
2025-06-21 08:34:20 +0300

    oop encapsulation

--- 4_oop/1_oop.py
+++ 4_oop/1_oop.py
@@ -40,3 +40,26 @@ class EnemyNew:
 
 zombie = EnemyNew(type_of_enemy='Zombie')
 zombie.talk()
+
+# Encapsulation - use __ before private fields + add getter and setters
+class EnemyEnc:
+    def __init__(self, type_of_enemy, health_points=100, attack_damage = 10):
+        self.__type_of_enemy = type_of_enemy
+        self.health_points = health_points
+        self.attack_damage = attack_damage
+
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
+    
+    def talk(self):
+        print(f'I am a {self.__type_of_enemy}. Be prepared to fight!')
+
+    def walk_forward(self):
+        print(f'{self.__type_of_enemy} moves closer to you.')
+
+    def attack(self):
+        print(f'{self.__type_of_enemy} attacks for {self.attack_damage} damage')
+
+zombie = EnemyEnc(type_of_enemy='Zombie')
+zombie.talk()
+print(zombie.get_type_of_enemy())
