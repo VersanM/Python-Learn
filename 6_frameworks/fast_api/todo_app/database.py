55c5cae3589767a543231bc895cc29fac9b8ed08
VersanMihai <versan.mihai@gmail.com>
2025-06-27 09:39:38 +0300

    added mysql connection as comment

--- 6_frameworks/fast_api/todo_app/database.py
+++ 6_frameworks/fast_api/todo_app/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # replace *** with database password
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:***@localhost/TodoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:***@127.0.0.1:3306/TodoApplicationDatabase'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
